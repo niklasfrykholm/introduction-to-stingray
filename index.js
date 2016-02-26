8625fc3ef52895d04ae698da92c14540f9f9c5f7
Niklas Frykholm <niklas.frykholm@autodesk.com>
2016-02-26 09:19:23 +0100

    Bigger preview.

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function render()
 
     const root = e("div", {});
     if (state.view == "list") {
-        const w = 200 * state.aspectRatio, h = 200;
+        const w = 300 * state.aspectRatio, h = 300;
         let x = 0, y = 0;
         for (let i=0; i<slides.length; ++i) {
             const div = addDiv(root, {left: x, top: y, width: w, height: h});
